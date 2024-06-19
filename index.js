ee2e1e538b7f35ac584ab9643cab4abdff0cc6d3
HprogrammerArif <programmerhabluarif@gmail.com>
2024-06-19 11:08:29 +0600

    create a new api for store new user in db and their role

--- index.js
+++ index.js
@@ -1,40 +1,47 @@
-const express = require('express')
-const app = express()
-require('dotenv').config()
-const cors = require('cors')
-const cookieParser = require('cookie-parser')
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb')
-const jwt = require('jsonwebtoken')
+const express = require("express");
+const app = express();
+require("dotenv").config();
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const jwt = require("jsonwebtoken");
 
-const port = process.env.PORT || 8000
+const port = process.env.PORT || 8000;
+
 
 // middleware
 const corsOptions = {
-  origin: ['http://localhost:5173', 'http://localhost:5174'],
+  origin: [
+    'http://localhost:5173',
+    'http://localhost:5174',
+    // "stayvision-e5db4.web.app",
+    // "stayvision-e5db4.firebaseapp.com",
+  ],
   credentials: true,
   optionSuccessStatus: 200,
-}
-app.use(cors(corsOptions))
+};
+app.use(cors(corsOptions));
 
-app.use(express.json())
-app.use(cookieParser())
+app.use(express.json());
+app.use(cookieParser());
 
 // Verify Token Middleware
 const verifyToken = async (req, res, next) => {
-  const token = req.cookies?.token
-  console.log(token)
+  const token = req.cookies?.token;
+  //console.log(token);
   if (!token) {
-    return res.status(401).send({ message: 'unauthorized access' })
+    return res.status(401).send({ message: "unauthorized access" });
   }
   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
     if (err) {
-      console.log(err)
-      return res.status(401).send({ message: 'unauthorized access' })
+      //console.log(err);
+      return res.status(401).send({ message: "unauthorized access" });
     }
-    req.user = decoded
-    next()
-  })
-}
+    req.user = decoded;
+    next();
+  });
+};
+
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.epjsucj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
@@ -46,65 +53,131 @@ const client = new MongoClient(uri, {
     strict: true,
     deprecationErrors: true,
   },
-})
+});
+
 
 async function run() {
   try {
+     // const db = client.db('stayvista')
+    // const roomsCollection = db.collection('rooms')
+    // const usersCollection = db.collection('users')
+    // const bookingsCollection = db.collection('bookings')
 
-    const studySessionCollection = client.db('stayvision').collection('session')
+    const db = client.db('stayvision')
+    const studySessionCollection = db.collection('session')
+    const usersCollection = db.collection('users')
+
 
     // auth related api
-    app.post('/jwt', async (req, res) => {
-      const user = req.body
+    app.post("/jwt", async (req, res) => {
+      const user = req.body;
       const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
-        expiresIn: '365d',
-      })
+        expiresIn: "365d",
+      });
       res
-        .cookie('token', token, {
+        .cookie("token", token, {
           httpOnly: true,
-          secure: process.env.NODE_ENV === 'production',
-          sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+          secure: process.env.NODE_ENV === "production",
+          sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
         })
-        .send({ success: true })
-    })
+        .send({ success: true });
+    });
+
+
     // Logout
-    app.get('/logout', async (req, res) => {
+    app.get("/logout", async (req, res) => {
       try {
         res
-          .clearCookie('token', {
+          .clearCookie("token", {
             maxAge: 0,
-            secure: process.env.NODE_ENV === 'production',
-            sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+            secure: process.env.NODE_ENV === "production",
+            sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
           })
-          .send({ success: true })
-        console.log('Logout successful')
+          .send({ success: true });
+        //console.log("Logout successful");
       } catch (err) {
-        res.status(500).send(err)
+        res.status(500).send(err);
       }
-    })
+    });
 
-    // get all study session form db
-    app.get('/session', async (req, res) => {
-      const result = await studySessionCollection.find().toArray()
+
+
+    // save a user data in db
+    app.put('/user', async (req, res) => {
+      const user = req.body
+      //console.log(user);
+      
+      const query = { email: user?.email }
+      // check if user already exists in db
+      const isExist = await usersCollection.findOne(query)
+      if (isExist) {
+
+        // if (user.status === 'Requested') {
+        //   // if existing user try to change his role
+        //   const result = await usersCollection.updateOne(query, {
+        //     $set: { status: user?.status },
+        //   })
+        //   return res.send(result)
+        // } else {
+        //   // if existing user login again
+        //   return res.send(isExist)
+        // }
+
+        return res.send(isExist)
+      }
+
+      // save user for the first time
+      const options = { upsert: true }
+      const updateDoc = {
+        $set: {
+          ...user,
+          timestamp: Date.now(),
+        },
+      }
+      const result = await usersCollection.updateOne(query, updateDoc, options)
+
+      // // welcome new user
+      // sendEmail(user?.email, {
+      //   subject: 'Welcome to Stayvista!',
+      //   message: `Hope you will find you destination`,
+      // })
+
+
       res.send(result)
     })
-    
+
+
+    // get all study session form db
+    app.get("/session", async (req, res) => {
+      const result = await studySessionCollection.find().toArray();
+      res.send(result);
+    });
+
+
+
+
+
 
     // Send a ping to confirm a successful connection
-    await client.db('admin').command({ ping: 1 })
+    await client.db("admin").command({ ping: 1 });
     console.log(
-      'Pinged your deployment. You successfully connected to MongoDB!'
-    )
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
   }
 }
-run().catch(console.dir)
+run().catch(console.dir);
 
-app.get('/', (req, res) => {
-  res.send('Hello from StayVista Server..')
-})
+
+
+
+
+
+app.get("/", (req, res) => {
+  res.send("Hello from StayVista Server..");
+});
 
 app.listen(port, () => {
-  console.log(`StayVista is running on port ${port}`)
-})
+  console.log(`StayVista is running on port ${port}`);
+});
